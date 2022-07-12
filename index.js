b526b98615893e4cb800cef148bccb176717b0b6
lucianorivadeo <luciano.rivadeogomez@hotmail.com>
2022-07-11 23:35:19 -0300

    entrega2

--- index.js
+++ index.js
@@ -9,19 +9,3 @@ let descuentos = 13000
 let precioFinal01 = resta(suma(precioCemento, iva(precioCemento)), descuentos )
 console.log (precioFinal01)
 */
-
-
-
-
-function calcularPrecio (precioCemento, cantidadCemento, descuentoContado) {
-     let descuento = (precioCemento * descuentoContado) / 100;
-     let precioConDescuento = precioCemento - descuentoContado;    
-    return (descuentoContado * cantidadCemento);
-}
-let cemento = parseFloat(prompt("Ingresar precio cemento:"));
-let cantidad = parseInt(prompt("Ingresar cantidad a comprar"));
-let descuento = parseInt(prompt("Ingresar descuento"))  
-
-//Precio final por pago efectivo
-let precioFinal = calcularPrecio(precioCemento, cantidadCemento, descuentoContado);
-alert("A pagar" + precioFinal);
